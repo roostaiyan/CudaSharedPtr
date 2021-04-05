ad6514e70f38cb6bd768fb7a7dde083dfce0b113
Seyed Mahdi Roostaiyan <mips00@gmail.com>
2021-04-06 00:17:19 +0430

    Update cudasharedptr.h
    fun::cuda namespace instead of cuda namespace (which is used in opencv)

--- cudasharedptr.h
+++ cudasharedptr.h
@@ -22,29 +22,36 @@
 namespace inner_implementation {
 template <typename T_ELEM>
 struct CudaPtrWrapper {
-    size_t n_elements;
+    int n_elements;
     T_ELEM* data = nullptr;
+    cudaError_t status;
 
 public:
-    void upload(const T_ELEM* data_arr, size_t size, cudaStream_t stream){
+    bool create(size_t size){
+        if(n_elements == size)
+            return true;
         destroy();
+        if(size<=0)
+            return false;
         n_elements = size;
-        if(n_elements<=0)
-            return;
         size_t buffer_size = n_elements*sizeof(T_ELEM);
-        cudaError_t status = cudaMalloc(&data, buffer_size);
+        status = cudaMalloc(&data, buffer_size);
         if(status!=cudaSuccess){
             std::cout << "Error: cudaMalloc was failed " << std::endl;
-            return;
+            return false;
         }
+        return true;
+    }
+    void upload(const T_ELEM* data_arr, size_t size, cudaStream_t stream){
+        if(!create(size))
+            return;
+        size_t buffer_size = n_elements*sizeof(T_ELEM);
         if(stream)
             status = cudaMemcpyAsync(data, data_arr, buffer_size, cudaMemcpyHostToDevice, stream);
         else
             status = cudaMemcpy(data, data_arr, buffer_size, cudaMemcpyHostToDevice);
         if(status!=cudaSuccess){
             std::cout << "Error: cudaMemcpy was failed " << std::endl;
-            if(data)
-                cudaFree(data);
             return;
         }
     }
@@ -64,6 +71,7 @@ public:
             return;
         }
     }
+
     void destroy(){
         if(data){
             cudaFree(data);
@@ -77,6 +85,8 @@ public:
 };
 }
 
+namespace fun {
+
 namespace cuda {
 
 template <typename T_ELEM>
@@ -87,6 +97,10 @@ public:
     shared_ptr(){
         dev_array = std::make_shared<inner_implementation::CudaPtrWrapper<T_ELEM>>();
     }
+    shared_ptr(size_t n_elements){
+        dev_array = std::make_shared<inner_implementation::CudaPtrWrapper<T_ELEM>>();
+        create(n_elements);
+    }
     ~shared_ptr(){
         if(dev_array)
             dev_array.reset();
@@ -94,11 +108,17 @@ public:
     T_ELEM* data(){
         return dev_array->data;
     }
-    size_t size(){
+    const T_ELEM* data() const {
+        return dev_array->data;
+    }
+    int size(){
         return dev_array->n_elements;
     }
+    bool create(size_t n_elements){
+        return dev_array->create(n_elements);
+    }
     void upload_async(const std::vector<T_ELEM> &data_vec, cudaStream_t stream){
-        int n_elements = data_vec.size();
+        size_t n_elements = data_vec.size();
         std::unique_ptr<T_ELEM[]> data_arr = std::unique_ptr<T_ELEM[]>(new T_ELEM[n_elements]);
         std::copy(data_vec.begin(), data_vec.end(), data_arr.get());
         dev_array->upload(data_arr.get(), n_elements, stream);
@@ -107,20 +127,22 @@ public:
         upload_async(data_vec, nullptr);
     }
     void download_async(std::vector<T_ELEM> &data_vec, cudaStream_t stream) const {
-        int n_elements = dev_array->n_elements;
+        size_t n_elements = dev_array->n_elements;
         if(n_elements<=0)
             return;
         std::unique_ptr<T_ELEM[]> data_arr = std::unique_ptr<T_ELEM[]>(new T_ELEM[n_elements]);
         dev_array->download(data_arr.get(), stream);
+        if(stream)
+            cudaStreamSynchronize(stream);
         std::copy(data_arr.get(), data_arr.get()+n_elements, data_vec.begin());
     }
     void download(std::vector<T_ELEM> &data_vec) const {
         download_async(data_vec, nullptr);
     }
-    void upload_async(const T_ELEM* data_arr, int n_elements, cudaStream_t stream){
+    void upload_async(const T_ELEM* data_arr, size_t n_elements, cudaStream_t stream){
         dev_array->upload(data_arr, n_elements, stream);
     }
-    void upload(const T_ELEM* data_arr, int n_elements){
+    void upload(const T_ELEM* data_arr, size_t n_elements){
         upload_async(data_arr, n_elements, nullptr);
     }
     void download_async(T_ELEM* data_vec, cudaStream_t stream) const {
@@ -132,5 +154,5 @@ public:
 };
 }
 
+}
 #endif // CUDASHAREDPTR_H
-
